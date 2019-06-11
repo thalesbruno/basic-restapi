97d7c2b12c2afbcd23c8e269465ec3fc04e11fd0
Thales Bruno <thalesbrunom@gmail.com>
2019-06-11 05:47:40 -0300

    adding routes and controllers

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ const app = express()
 mongoose.connect('mongodb://localhost:27017/db-myapp',{ useNewUrlParser: true })
 require('./src/models/Product')
 
-const Product = mongoose.model('Product')
+app.use('/api/v1', require('./src/routes'))
 
-app.get('/', (req, res) => {
-  Product.create({ 
-    title: 'Some product',
-    description: 'Product with great value',
-    url: 'https://product.com'
-   })
-  return res.send('Virtual Store')
-}) 
-
-app.listen(30000)
+app.listen(3001)
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const ProductController = require('./controllers/ProductController')
+
+const routes = express.Router()
+
+routes.get('/products', ProductController.index)
+
+module.exports = routes
